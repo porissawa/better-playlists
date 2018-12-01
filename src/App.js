817e7a57e71e5cf629c2ace002038a4f29844abc
porissawa <porissawa@users.noreply.github.com>
2018-11-30 22:00:10 -0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import './App.css';
+
+let defaultTextColor = '#fff'
+let defaultStyle = {
+  color: defaultTextColor
+}
+
+class Aggregate extends Component {
+  render() {
+    return(
+      <div className='aggregate' style={{...defaultStyle, width: '40%', display: 'inline-block'}}>
+        <h2>Number Text</h2>
+      </div>
+    )
+  }
+}
+
+class Filter extends Component {
+  render() {
+    return(
+      <div style={defaultStyle}>
+        <img/>
+        <input type='text'/> Filter
+      </div>
+    )
+  }
+}
+
+class Playlist extends Component {
+  render() {
+    return(
+      <div style={{...defaultStyle, width: '20%', display: 'inline-block'}}>
+        <img/>
+        <h3>Playlist Name</h3>
+        <ul>
+          <li>Song 1</li>
+          <li>Song 2</li>
+          <li>Song 3</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+         <h1>Title</h1>
+         <Aggregate />
+         <Aggregate />
+         <Filter />
+         <Playlist />
+         <Playlist />
+         <Playlist />
+         <Playlist />
+      </div>
+    )
+  }
+}
+
+
+
+export default App
